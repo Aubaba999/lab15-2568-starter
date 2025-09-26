656763466cc855b247cf022dc5710ce50713f612
Aubaba999 <sutjii1989@gmail.com>
2025-09-26 10:14:49 +0700

    1

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,37 @@
+import { Router, type Request, type Response } from "express";
 import express from "express";
 import morgan from 'morgan';
-
+import router from "./routes/studentRoutes.js";
+import { json } from "node:stream/consumers";
+import courseRouter from "./routes/courseRoutes.js";
 const app: any = express();
 
 //Middleware
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.get("/", (req : Request, res: Response) => {
+ return res.json({"success":true,"message":"lab 15 API service successfully"})
+})
+app.use("/api/v2/",router);
+app.use("/api/v2/",courseRouter);
+app.get("/me", (req: Request, res: Response) => {
+
+return res.json(
+ {
+  "success": true,
+  "message": "Student Information",
+  "data": {
+    "studentId": "670610675",
+    "firstName": "Kasamesak",
+    "lastName": "Suttajai",
+    "program": "CPE",
+    "section": "001"
+  }
+}
+)
+
+});
 app.listen(3000, () =>
   console.log("🚀 Server running on http://localhost:3000")
 );
